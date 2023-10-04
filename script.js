e265c316314d5aa8f7b5cdb5c6f8419f4fbb8f24
alexy piche <alexyautumn@gmail.com>
2023-10-04 19:33:03 -0400

    Ajusted script

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 function calculateAge() {
-    const yearsInput = parseInt(document.getElementById("birthYear").value);
-    const monthsInput = parseInt(document.getElementById("birthMonth").value);
-    const daysInput = parseInt(document.getElementById("birthDay").value);
+    let d1 = document.getElementById("birthDay").value;
+    let m1 = document.getElementById("birthMonth").value;
+    let y1 = document.getElementById("birthYear").value;
+    let inputElement = document.getElementById("input-container");
 
-    const currentDate = new Date();
-    const birthdate = new Date(currentDate.getFullYear() - yearsInput, currentDate.getMonth() - monthsInput, currentDate.getDate() - daysInput);
+    if(d1 ==='' | m1 === '' || y1 === '') {
+        inputElement.classList.add("error");
+        return;
+    }
 
-    const ageMilliseconds = currentDate - birthdate;
-    const ageDate = new Date(ageMilliseconds);
-  
-    const ageYears = ageDate.getUTCFullYear() - 1970;
-    const ageMonths = ageDate.getUTCMonth();
-    const ageDays = ageDate.getUTCDate() -1;
+    let date = new Date();
+    let d2 = date.getDate();
+    let m2 = 1 + date.getMonth();
+    let y2 = date.getFullYear();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
-        ageYears--;
-        ageMonths += 12;
-    
-        if (ageDays < 0) {
-          const lastDayOfMonth = new Date(
-            currentDate.getFullYear(),
-            currentDate.getMonth(),
-            0
-          ).getDate();
-          ageDays += lastDayOfMonth;
-          ageMonths--;
-        }
+    if(d1 > d2){
+        d2 = d2 + month[m2 - 1];
+        m2 = m2 - 1;
+      }
+      if(m1 > m2){
+        m2 = m2 + 12;
+        y2 = y2 - 1;
       }
 
-    const yearsResultDiv = document.getElementById("yearsResult");
-    const monthsResultDiv = document.getElementById("monthsResult");
-    const daysResultDiv = document.getElementById("daysResult");
+    let d = d2 - d1;
+    let m = m2 - m1;
+    let y = (y2 - y1);
 
-    yearsResultDiv.innerHTML = ageYears;
-    monthsResultDiv.innerHTML = ageMonths;
-    daysResultDiv.innerHTML = ageDays;
+    let yearsResultDiv = document.getElementById("yearsResult");
+    let monthsResultDiv = document.getElementById("monthsResult");
+    let daysResultDiv = document.getElementById("daysResult");
 
-    console.log(currentDate);
-    console.log(ageMilliseconds);
-    console.log(ageDate);
+    yearsResultDiv.innerHTML = y;
+    monthsResultDiv.innerHTML = m;
+    daysResultDiv.innerHTML = d;
 
+    inputElement.classList.remove("error");
+     
 }
